22f08c51fd478101348bba563b2c25d04fb6fe4d
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-03-13 16:56:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # Load file
-## import class StationInfo 
+## import StationInfo from LoadData.py
+from LoadData import StationInfo
 
 # Collect user from_station and to_station
 from_station = input("Where is your current station? (Example input: AldgateEast or aldgateeast): \n" )
 to_station = input("Where you want to go? (Example input: AldgateEast or aldgateeast): \n")
 
+# Use StationInfo to get the station information
+from_station_id = StationInfo.GetIDFromName(from_station)
+to_station_id = StationInfo.GetIDFromName(to_station)
+from_station_nei = StationInfo.GetNeighboursFromID(from_station)
+
+# Import StationFinder to find the path
+## Take the from_station_id as root node to create a tree using StationTree and StationNode
