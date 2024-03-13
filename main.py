b4b7a6388660a0aa08dee1a0b0aae04d945057ca
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-03-13 15:13:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-```py
+
 # Load file
 ## import class StationInfo 
 
 # Collect user from_station and to_station
 from_station = input("Where is your current station? (Example input: AldgateEast or aldgateeast): \n" )
 to_station = input("Where you want to go? (Example input: AldgateEast or aldgateeast): \n")
-```
+
