b298ff9affe3efb2cb42a73a3975219ac939ab45
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-03-17 10:42:12 +0000

    Update main.py

--- ShortestPathInLondonUnderground/main.py
+++ ShortestPathInLondonUnderground/main.py
@@ -1,1 +1,24 @@
+# Load file
+## import StationInfo from LoadData
+## import StationFinder from PathFinder
+from LoadData import StationInfo
+from PathFinder import StationFinder
+while True:
+    # Collect user from_station and to_station
+    from_station = input("Where is your start station? (Example input: AldgateEast): \n" )
+    to_station = input("Where you want to go? (Example input: AldgateEast): \n")
 
+    # Check if the input is in the StationInfo
+    if from_station in StationInfo.m_name_to_id_dict and to_station in StationInfo.m_name_to_id_dict:
+        from_station_id = StationInfo.GetIDFromName(from_station)
+        to_station_id = StationInfo.GetIDFromName(to_station)
+    else:
+        print(f"Your station input is not correct, try to input again.")
+        continue
+    ## Use StationFinder to find the path
+    StationFinder.GetPaths(from_station_id, to_station_id, from_station, to_station)
+    
+    ## Ask if the user want to find another path
+    if_continue = input("Would you like to use our service again? Yes or No:\n").lower()
+    if if_continue == "no":
+        break
