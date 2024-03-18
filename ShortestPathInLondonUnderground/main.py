cec0a861e89e0835db7b99cbeb5e6e82d2e48b37
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-03-18 23:42:02 +0000

    Update main.py

--- ShortestPathInLondonUnderground/main.py
+++ ShortestPathInLondonUnderground/main.py
@@ -1,23 +1,26 @@
 # Load file
-## import StationInfo from LoadData
-## import StationFinder from PathFinder
+## Import StationInfo from LoadData
+## Import StationFinder from PathFinder
 from LoadData import StationInfo
 from PathFinder import StationFinder
+## Find the path of the file, here the file is stations.txt, you can change the path of your own stations file.
+filename = "/Users/fanfan/Downloads/200-Learning/250-Com/Pycharm/pythonProject/LondonTube/stations.txt"
+## Pass the file to StationInfo to convert the txt file to dictionary
+StationInfo.LoadData(filename)
+
 while True:
     # Collect user from_station and to_station
     from_station = input("Where is your start station? (Example input: AldgateEast): \n" )
     to_station = input("Where you want to go? (Example input: AldgateEast): \n")
 
     # Check if the input is in the StationInfo
-    if from_station in StationInfo.m_name_to_id_dict and to_station in StationInfo.m_name_to_id_dict:
-        from_station_id = StationInfo.GetIDFromName(from_station)
-        to_station_id = StationInfo.GetIDFromName(to_station)
-    else:
+    if from_station not in StationInfo.GetNameIDDict() or to_station not in StationInfo.GetNameIDDict():
         print(f"Your station input is not correct, try to input again.")
         continue
     ## Use StationFinder to find the path
-    StationFinder.GetPaths(from_station_id, to_station_id, from_station, to_station)
-    
+    paths = StationFinder.GetPaths(from_station, to_station)
+    StationFinder.DisplayPaths(paths)
+
     ## Ask if the user want to find another path
     if_continue = input("Would you like to use our service again? Yes or No:\n").lower()
     if if_continue == "no":
