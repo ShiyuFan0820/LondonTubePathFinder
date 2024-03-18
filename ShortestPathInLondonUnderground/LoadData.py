4a4193cb54c7aa29be95d1310e9b396860a73c97
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-03-18 11:18:31 +0000

    Update LoadData.py

--- ShortestPathInLondonUnderground/LoadData.py
+++ ShortestPathInLondonUnderground/LoadData.py
@@ -1,12 +1,17 @@
 import pandas as pd
-filename = "stations.txt"
 
 
 class StationInfo:
-    m_data = pd.read_csv(filename, delimiter="|", index_col=False)
-    m_id_to_name_dict = dict(zip(m_data["ID"], m_data["Name"]))
-    m_name_to_id_dict = dict(zip(m_data["Name"], m_data["ID"]))
-    m_id_neighbours_dict = {row["ID"]: [[int(id) for id in row["TubeNeighbour"].split(",")], [int(id) for id in row["TrainNeighbour"].split(",")]] for index, row in m_data.iterrows()}
+    m_name_to_id_dict = None
+    m_id_to_name_dict = None
+    m_id_neighbours_dict = None
+
+    @classmethod
+    def LoadData(cls, filename):
+        data = pd.read_csv(filename, delimiter="|", index_col=False)
+        cls.m_name_to_id_dict = dict(zip(data["Name"], data["ID"]))
+        cls.m_id_to_name_dict = dict(zip(data["ID"], data["Name"]))
+        cls.m_id_neighbours_dict = {row["ID"]: [[int(id) for id in row["TubeNeighbour"].split(",")], [int(id) for id in row["TrainNeighbour"].split(",")]] for index, row in data.iterrows()}
 
     @classmethod
     def GetNameFromID(cls, id_num):
@@ -17,5 +22,9 @@ class StationInfo:
         return cls.m_name_to_id_dict[name]
 
     @classmethod
-    def GetNeighboursFromID(cls, id_num):
+    def GetTubeNeisFromID(cls, id_num):
         return cls.m_id_neighbours_dict[id_num][0]
+
+    @classmethod
+    def GetTrainNeisFromID(cls, id_num):
+        return cls.m_id_neighbours_dict[id_num][1]
