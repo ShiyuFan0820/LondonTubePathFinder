5427b090219bd00a559968dc1a99ebaabb9e8a77
Shiyu Fan <149340606+ShiyuFan0820@users.noreply.github.com>
2024-03-25 10:20:54 +0000

    Update main.py

--- CompletedCodeOfShortestPathInLondonUnderground/main.py
+++ CompletedCodeOfShortestPathInLondonUnderground/main.py
@@ -8,26 +8,26 @@ filename = "/Users/fanfan/Downloads/200-Learning/250-Com/Pycharm/pythonProject/L
 ## Pass the file to StationInfo to convert the txt file to dictionary
 StationInfo.LoadData(filename)
 
+if __name__ == "__main__":
+    while True:
+        # Collect user from_station and to_station from UI input
+        from_station = input("Where is your start station? (Example input: AldgateEast): \n" )
+        to_station = input("Where you want to go? (Example input: AldgateEast): \n")
 
-while True:
-    # Collect user from_station and to_station from UI input
-    from_station = input("Where is your start station? (Example input: AldgateEast): \n" )
-    to_station = input("Where you want to go? (Example input: AldgateEast): \n")
+        # Check if the input is in the StationInfo
+        if from_station not in StationInfo.GetNameIDDict() or to_station not in StationInfo.GetNameIDDict():
+            print(f"Your station input is not correct, try to input again.")
+            continue
+        ## Use StationFinder to find the path
+        paths = StationFinder.GetPaths(from_station, to_station)
+        if paths:
+            StationFinder.DisplayAllPaths(paths)
+            shortest_path = StationFinder.ShortestPath(paths)
+            StationFinder.DisplayShortestPath(shortest_path)
+        else:
+            print("Sorry, no path found.")
 
-    # Check if the input is in the StationInfo
-    if from_station not in StationInfo.GetNameIDDict() or to_station not in StationInfo.GetNameIDDict():
-        print(f"Your station input is not correct, try to input again.")
-        continue
-    ## Use StationFinder to find the path
-    paths = StationFinder.GetPaths(from_station, to_station)
-    if paths:
-        StationFinder.DisplayAllPaths(paths)
-        shortest_path = StationFinder.ShortestPath(paths)
-        StationFinder.DisplayShortestPath(shortest_path)
-    else:
-        print("Sorry, no path found.")
-
-    ## Ask if the user want to find another path
-    if_continue = input("Would you like to use our service again? Yes or No:\n").lower()
-    if if_continue == "no":
-        break
+        ## Ask if the user want to find another path
+        if_continue = input("Would you like to use our service again? Yes or No:\n").lower()
+        if if_continue == "no":
+            break
